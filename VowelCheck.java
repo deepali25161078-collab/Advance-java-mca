d66de630541e58a92df979273ba50985557d2c0e
deepali25161078-collab <deepali.25161078@kiet.edu>
2026-04-18 22:49:47 +0530

    Add files via upload

---
+++ VowelCheck.java
@@ -0,0 +1,17 @@
+public class VowelCheck {
+public static void main(String[] args){
+    char ch='a';
+    switch (ch) {
+
+        case 'a':
+        case 'e':
+            case 'i':
+                case 'o':
+                    case 'u':
+                        System.out.println("vowel");
+                        break;
+                        default:
+                            System.out.println("Not a vowel");
+}
+}  
+}
