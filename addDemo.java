65db28a81b66bbeeb42f56c9a524be8ac4017543
deepali25161078-collab <deepali.25161078@kiet.edu>
2026-04-18 22:29:27 +0530

    Add files via upload

---
+++ addDemo.java
@@ -0,0 +1,9 @@
+public class addDemo {
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static void main(String [] args){
+        int result= add(10,20);
+        System.out.println("Addition " + result);
+    }
+}
